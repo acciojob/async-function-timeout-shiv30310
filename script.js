31514ab07e19a330d787ef47aebba238d94c7ca4
Gulshan Soni <43094983+shiv30310@users.noreply.github.com>
2023-06-28 12:37:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const output = document.getElementById("output")
+
+const userInput = document.getElementById("text")
+const delay = document.getElementById("delay")
+
+const submitBtn = document.getElementById("btn")
+
+submitBtn.addEventListener('click', onSubmit);
+
+function onSubmit(){
+    return new Promise(resolve => {
+        setTimeout(()=>{
+            output.innerText = userInput.value
+        }, delay)
+        submitBtn.removeEventListener('click', onSubmit);
+    })
+
+}
